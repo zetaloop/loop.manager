7c6b6b163fe245397e08995a29d510276b6933cd
zetaloop <ideaploter@outlook.com>
2023-06-29 11:15:55 +0800

    Update update.sh

--- scripts/loopmanager/update.sh
+++ scripts/loopmanager/update.sh
@@ -1,1 +1,4 @@
 # Update LoopManager from GitHub
+
+cd ..
+nohup bash -c 'wget https://github.com/zetaloop/Loop.Manager/archive/main.zip -O lom.zip && unzip -o lom.zip && rm lom.zip && cd Loop.Manager-main && python3 lom.py'
