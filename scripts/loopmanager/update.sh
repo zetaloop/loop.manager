f5ad634b7a94a1dc3a056e1d18fbcc7691c9f9fc
zetaloop <36418285+zetaloop@users.noreply.github.com>
2023-07-25 14:49:45 +0800

    Update update.sh

--- scripts/loopmanager/update.sh
+++ scripts/loopmanager/update.sh
@@ -5,4 +5,6 @@ wget https://github.com/zetaloop/Loop.Manager/archive/main.zip -O lom.zip
 unzip -o lom.zip
 rm lom.zip
 echo ====[Update Finished]====
+echo
 echo To start Loop.Manager, run: python3 Loop.Manager-main/lom.py 
+echo
