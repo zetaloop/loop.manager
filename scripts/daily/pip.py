36db4ff8d3caecdc1e1a9bb79250e6f7388342f8
zetaloop <36418285+zetaloop@users.noreply.github.com>
2023-07-26 14:15:15 +0800

    Update pip.py

--- scripts/daily/pip.py
+++ scripts/daily/pip.py
@@ -14,20 +14,19 @@ for i in jsondata:
 print('-' * 16)
 
 packages = allpackages
-errorpackages = []
 while True:
-    print('Try updating: ' + ' '.join(packages))
-    if os.system('pip install -U ' + ' '.join(packages)):
+    print('Try updating: ' + ' '.join(packages)
+    if os.system('pip install -U ' + ' '.join(packages):
         print('An error has occurred, checking...')
         pipcmd = 'pip install -U'.split() + packages
         result = subprocess.run(pipcmd, capture_output=True, text=True).stdout
         err = [i for i in result.split('\n') if i.startswith('Collecting')][-1].split(' ')[1]
         errorpackages += err
-        print('Try ignoring package: ' + ' '.join(errorpackages))
+        print('Try ignoring package: ' + ' '.join(errorpackages)
         erri = packages.index(err)
-        print('And updating: ' + ' '.join(packages[:erri]))
+        print('And updating: ' + ' '.join(packages[:erri])
         if os.system('pip install -U ' + ' '.join(packages[:erri])):
-            print('Another error has occured. Please solve the problem manually. Auto update stopped.')
+            print('Another error has occured. Please solve the problem manually. Auto update stopped.)
             exit()
         packages = packages[erri + 1:]
     else:
