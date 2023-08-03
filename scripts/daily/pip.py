fd57f6199744fb3f953753cfe3990a24cfa80c44
zetaloop <36418285+zetaloop@users.noreply.github.com>
2023-08-03 09:46:52 +0800

    Update pip.py

--- scripts/daily/pip.py
+++ scripts/daily/pip.py
@@ -1,4 +1,4 @@
-# Python pip packages update
+# Python pip packs update
 
 print('Updating pip...')
 import subprocess, json, os
@@ -7,7 +7,7 @@ os.system('python3 -m pip install -U pip')
 print('Checking pip outdated...')
 pipcmd = ["pip", "list", "--outdated", "--format", "json"]
 jsondata = json.loads(subprocess.run(pipcmd, capture_output=True, text=True).stdout)
-allpackages = [pkg['name'] for pkg in jsondata]
+allpacks = [pkg['name'] for pkg in jsondata]
 
 print('-' * 16)
 if not jsondata:
@@ -16,32 +16,43 @@ for i in jsondata:
     print(f"{i['name']} {i['version']} >>> {i['latest_version']}")
 print('-' * 16)
 
-packages = allpackages
-errorpackages = []
-while packages:
-    print('Try updating: ' + ' '.join(packages))
-    if os.system('pip install -U ' + ' '.join(packages)):
-        print('An error has occurred, checking...')
-        pipcmd = 'pip install -U'.split() + packages
-        result = subprocess.run(pipcmd, capture_output=True, text=True).stdout
-        err_lines = [i for i in result.split('\n') if i.startswith('Collecting')]
-        if err_lines:
-            err_line_parts = err_lines[-1].split(' ')
-            if err_line_parts:
-                err = err_line_parts[1]
-                errorpackages.append(err)
-                print('Try ignoring package: ' + ' '.join(errorpackages))
-                erri = packages.index(err)
-                if packages[:erri]:
-                    print('And updating: ' + ' '.join(packages[:erri]))
-                    if os.system('pip install -U ' + ' '.join(packages[:erri])):
-                        print('Another error has occured. Please solve the problem manually. Auto update stopped.')
-                        exit()
-                packages = packages[erri + 1:]
-    else:
-        break
+packs = allpacks
+error_packs = []
 
-installedpackages = [i for i in allpackages if i not in errorpackages]
-print(f'Update finished {len(installedpackages)}/{len(allpackages)}')
-print('Updated: ' + ' '.join(installedpackages))
-print('Ignored: ' + ' '.join(errorpackages))
+def errorhandler(error_pack):
+    global packs
+    error = packs.index(err)
+    print('Try ignoring pack: ' + ' '.join(error_pack))
+    error_packs.append(error_pack)
+    mainloop(packs[:error])
+    packs = packs[error + 1:]
+
+def check(result):
+    packs = [i for i in result.split('\n') if i.startswith('Collecting')]
+    if not packs:
+        print('Unexpected error!')
+        exit()
+    error_pack = packs[-1].split(' ')[1]
+    if [i for i in result if i.startswith('ERROR: Cannot uninstall')]:
+        print('Uninstall failed, try force install: ' + error_pack)
+        if not os.system('pip install -U --ignore-installed ' + error_pack):
+            return
+    errorhandler(error_pack)
+
+def mainloop(packs):
+    print('Try updating: ' + ' '.join(packs))
+    if os.system('pip install -U ' + ' '.join(packs)):
+        print('An error has occurred, checking...')
+        pipcmd = 'pip install -U'.split() + packs
+        result = subprocess.run(pipcmd, capture_output=True, text=True).stdout.split('\n')
+        check(result)
+    else:
+        packs.clear()
+
+while packs:
+    mainloop(packs)
+
+installed_packs = [i for i in allpacks if i not in error_packs]
+print(f'Update finished {len(installed_packs)}/{len(allpacks)}')
+print('Updated: ' + ' '.join(installed_packs))
+print('Ignored: ' + ' '.join(error_packs))
