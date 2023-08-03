c106aeccf50aa6f4292b1fbe42f8ac50b3ac5b01
zetaloop <36418285+zetaloop@users.noreply.github.com>
2023-08-03 08:31:50 +0800

    Update pip.py

--- scripts/daily/pip.py
+++ scripts/daily/pip.py
@@ -14,21 +14,26 @@ for i in jsondata:
 print('-' * 16)
 
 packages = allpackages
-while True:
-    print('Try updating: ' + ' '.join(packages)
-    if os.system('pip install -U ' + ' '.join(packages):
+errorpackages = []
+while packages:
+    print('Try updating: ' + ' '.join(packages))
+    if os.system('pip install -U ' + ' '.join(packages)):
         print('An error has occurred, checking...')
         pipcmd = 'pip install -U'.split() + packages
         result = subprocess.run(pipcmd, capture_output=True, text=True).stdout
-        err = [i for i in result.split('\n') if i.startswith('Collecting')][-1].split(' ')[1]
-        errorpackages += err
-        print('Try ignoring package: ' + ' '.join(errorpackages)
-        erri = packages.index(err)
-        print('And updating: ' + ' '.join(packages[:erri])
-        if os.system('pip install -U ' + ' '.join(packages[:erri])):
-            print('Another error has occured. Please solve the problem manually. Auto update stopped.)
-            exit()
-        packages = packages[erri + 1:]
+        err_lines = [i for i in result.split('\n') if i.startswith('Collecting')]
+        if err_lines:
+            err_line_parts = err_lines[-1].split(' ')
+            if err_line_parts:
+                err = err_line_parts[1]
+                errorpackages.append(err)
+                print('Try ignoring package: ' + ' '.join(errorpackages))
+                erri = packages.index(err)
+                print('And updating: ' + ' '.join(packages[:erri]))
+                if os.system('pip install -U ' + ' '.join(packages[:erri])):
+                    print('Another error has occured. Please solve the problem manually. Auto update stopped.')
+                    exit()
+                packages = packages[erri + 1:]
     else:
         break
 
